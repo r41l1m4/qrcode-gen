e00115dba8959fbcf46573bd6a644c98c68ecb16
r41l1m4 <rai.lima17@gmail.com>
2025-05-29 18:19:33 -0300

    initial commit

---
+++ src/test/java/com/ironia/qrcodegen/QrcodeGenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironia.qrcodegen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeGenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
